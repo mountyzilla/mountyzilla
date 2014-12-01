6a109c9ee29ad1521a45bf25cf61c514a8879a7a
Dabihul (79738) <tontondabi@sfr.fr>
2014-12-01 21:10:34 +0100

    Nettoyage des missions terminées (patch dégueu)

---
+++ scripts_online/mission_liste_FF.js
@@ -0,0 +1,35 @@
+/*
+ * This file is part of MountyZilla (http://mountyzilla.tilk.info/),
+ * published under GNU License v2.
+ *
+ * Patch :
+ * gestion des missions terminées
+ */
+
+function checkLesMimis() {
+	try {
+		var titresMimis = document.evaluate(
+			"//div[@class='mh_titre3']/b/a[contains(@href,'Mission_')]",
+			document, null, 7, null
+		);
+		var obMissions = JSON.parse(MZ_getValue(numTroll+'.MISSIONS'));
+	} catch(e) {
+		console.error('[MZ mission_liste] Erreur initialisation:\n'+e);
+		return;
+	}
+	
+	var enCours = {};
+	for(var i=0 ; i<titresMimis.snapshotLength ; i++) {
+		var num = titresMimis.snapshotItem(i).textContent.match(/\d+/);
+		enCours[num] = true;
+	}
+	
+	for(var numMimi in obMissions) {
+		if(!enCours[numMimi]) {
+			delete obMissions[numMimi];
+		}
+	}
+	MZ_setValue(numTroll+'.MISSIONS',JSON.stringify(obMissions));
+}
+
+checkLesMimis();
