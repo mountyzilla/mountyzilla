99426e20016bfedace3264d8d87a205d123e715a
alainarachnid <alainarachnid@brion.fr>
2017-01-14 22:10:46 +0100

    Intermédiaire dev carte

--- tout_MZ.user.js
+++ tout_MZ.user.js
@@ -752,7 +752,7 @@ function ini_canvas_carte_hall() {
 * Usage:
 *	gliss = new glissiere_MZ(ref, labelHTML, target, bDynamic);
 *		ref : utilisé pour diversifier les IDs HTML
-*		labelHTML : le label qui apparaît davant la glissière (peut contenir des balises HTML)
+*		labelHTML : le label qui apparaît devant la glissière (peut contenir des balises HTML)
 *		target : peut être de 3 types
 *			- élément HTML : l'élément HTML à zoomer
 *			- string : l'ID de l'élément à zoomer (qui doit exister au moment de la création de la glissière
@@ -770,18 +770,29 @@ function glissiere_MZ(ref, labelHTML, paramTarget, bDynamic, valDef, valMin, val
 		var mouseDown = false;
 		var div_gliss = document.createElement("div");	// la DIV mère
 		div_gliss.id = "MZ_gliss_"+ref;
-		var div_label = document.createElement("div");	// le label
+		var div_label = document.createElement("span");	// le label
 		div_label.innerHTML = labelHTML;
 		div_gliss.appendChild(div_label);
 		div_gliss.className = "choix_zoom";
 		var dessin = document.createElement("canvas");	// le dessin lui-même
 		dessin.id = "MZ_gliss_dessin_"+ref;
+		dessin.style.cursor = "pointer";
 		dessin.width = 104;
 		dessin.height = 12;
+		dessin.style.marginLeft = '2px';
+		dessin.style.marginRight = '2px';
 		div_gliss.appendChild(dessin);
 		var pourcent = document.createElement("span");	// le pourcentage
 		pourcent.id = "MZ_gliss_pourcent_"+ref;
 		var pourcent_text = document.createTextNode('');
+		var previousVal;
+		var flouPourCurseurDoubleFleche = (valMax - valMin) / 40;
+
+		// alignement vertical
+		dessin.style.verticalAlign = 'middle';
+		div_label.style.verticalAlign = 'middle';
+		pourcent.style.verticalAlign = 'middle';
+
 		pourcent.appendChild(pourcent_text);
 		div_gliss.appendChild(pourcent);
 
@@ -815,10 +826,11 @@ function glissiere_MZ(ref, labelHTML, paramTarget, bDynamic, valDef, valMin, val
 			ctx.fillStyle = "rgb(200,200,200)";
 			ctx.fillRect(pos_0_100+1,1,3,10);
 			pourcent_text.nodeValue = val+'%';
+			previousVal = parseInt(val);
 		};
 
 		////////////////////////////////////
-		// action qur mousedown et mousemove
+		// action sur mousedown et mousemove
 		//		stocker la nouvelle valeur
 		//		redessiner
 		//		afficher la nouvelle valeur
@@ -837,7 +849,8 @@ function glissiere_MZ(ref, labelHTML, paramTarget, bDynamic, valDef, valMin, val
 					var xpos = evt.pageX;
 					var ypos = evt.pageY;
 				}
-				var val = Math.min(valMax,Math.max(valMin,((xsouris-1) * (valMax - valMin) / 100)+valMin));
+				var val = Math.floor(Math.min(valMax,Math.max(valMin,((xsouris-1) * (valMax - valMin) / 100)+valMin)));
+				dessin.style.cursor = (val <= (previousVal+flouPourCurseurDoubleFleche) && val >= (previousVal-flouPourCurseurDoubleFleche)) ? "e-resize":"pointer";
 				//		afficher la nouvelle valeur dans la bulle
 				bulle_pourcent_text.nodeValue = val + '%';
 				bulle_pourcent.style.top = (ypos+3)+'px';
@@ -863,7 +876,7 @@ function glissiere_MZ(ref, labelHTML, paramTarget, bDynamic, valDef, valMin, val
 					paramTarget(val);
 					return;
 				}
-				// à faire : zoom de l'elt
+				if (elt.setZoom != undefined) elt.setZoom(val);
 			} catch (e) {window.console.log(traceStack(e, 'glissiere_MZ.doCallback'))}
 		};
 
@@ -889,6 +902,258 @@ function glissiere_MZ(ref, labelHTML, paramTarget, bDynamic, valDef, valMin, val
 		if (val_init === undefined) val_init = valDef;
 		dessine_glissiere(val_init);
 	} catch (e) {window.console.log(traceStack(e, 'glissiere_MZ'))}
+}
+
+// calcul du point intermédiaire de déplacement gowap (x et y uniquement)
+// reçoit 2 objets avec des propriétés x et y
+// rend un objet avec x et y (rend undefined si le trajet est direct)
+function pointIntermediaireMonstre2D(locDepart, locArrivee) {
+	var deltaX = locArrivee.x - locDepart.x;
+	if (deltaX == 0) return; // pas de point intermédiaire
+	var deltaY = locArrivee.y - locDepart.y;
+	if (deltaY == 0) return; // pas de point intermédiaire
+	var absDeltaX = Math.abs(deltaX);
+	var absDeltaY = Math.abs(deltaY);
+	if (absDeltaX > absDeltaY) {
+		return {x: locDepart.x + Math.sign(deltaX) * Math.sign(deltaY) * deltaY, y: locArrivee.y};
+	} else if (absDeltaY > absDeltaX) {
+		return {x: locArrivee.x, y: locDepart.y + Math.sign(deltaX) * Math.sign(deltaY) * deltaX};
+	} else {
+		return;	// égalité, pas de point intermédiaire
+	}
+}
+
+/**********************
+* carte en mode objet 
+* Roule 14/01/2017 à partir du code des trajets gowap de Vapulabehemot
+*
+* Usage:
+*	carte = new carte_MZ(ref, tabDepl);
+*		ref : utilisé pour diversifier les IDs HTML
+*		tabDepl : table de tables d'objets contenant x, y et n (positions successives des différents suivants)
+*	autres méthodes
+*		carte.getElt();			// rend la div de la carte (par exemple pour la positioner dans la page)
+**********************/
+
+function carte_MZ(ref, tabDepl) {
+	try {
+		var div_carte = document.createElement("div");	// la DIV mère
+		div_carte.id = "MZ_carte_"+ref;
+		div_carte.className = "carte_MZ";
+		var dessin = document.createElement("canvas");	// le dessin lui-même
+		dessin.id = "MZ_carte_dessin_"+ref;
+		//dessin.style.backgroundColor = 'rgb(255,255,204)';
+		dessin.style.backgroundImage = 'url("/mountyhall/MH_Packs/packMH_parchemin/tableau/tableau1.jpg")';
+		div_carte.appendChild(dessin);
+
+		var position_trous_MZ = [[-70.5, -7.5, 2, 1.5, -69]	// x, y, ?, rayon du cercle, profondeur
+			, [-66.5, -37.5, 2, 1.5, -69]
+			, [-63.5, 8.5, 2, 1.5, -69]
+			, [-59.5, -32.5, 2, 1.5, -69]
+			, [-52, 57, 0.25, 0.8, -59]
+			, [-50.5, -22.5, 2, 1.5, -69]
+			, [-35.5, -51.5, 2, 1.5, -69]
+			, [-34.5, 14.5, 2, 1.5, -69]
+			, [-34.5, 64.5, 2, 1.5, -69]
+			, [-11.5, 72.5, 2, 1.5, -69]
+			, [5.5, -49.5, 2, 1.5, -69]
+			, [5.5, 31.5, 2, 1.5, -69]
+			, [10.5, 63.5, 2, 1.5, -69]
+			, [12, -15, 0.25, 0.8, -59]
+			, [21.5, 35.5, 2, 1.5, -69]
+			, [30, -52, 0.25, 0.8, -59]
+			, [46.5, 51.5, 2, 1.5, -69]
+			, [48, -39, 0.25, 0.8, -59]
+			, [55, 70, 0.25, 0.8, -59]	// correction Roule 10/10/2016 -59 au lieu de -69
+			, [56.5, 23.5, 75, 8.7, -99]
+			, [64, 70, 0.25, 0.8, -59]
+			, [74.5, 31.5, 2, 1.5, -69]];
+
+		var couleur_depl_normal = 'rgba(0,0,200,0.5)';
+		var couleur_cible = 'rgba(0,0,0,0.5)';
+		var couleur_depl_collision_trou = 'rgba(150,0,0, 0.5)';
+		var couleur_trou = 'rgb(200,0,0)';
+
+		var coord_x = function(val) {
+			return decalh+coeff*(val+100);
+		};
+		var coord_y = function(val) {
+			return decalv+coeff*(100-val);
+		};
+
+		var ctx = dessin.getContext('2d');
+		var coeff = MY_getValue("MZ_glissiere_" + ref);	// ce qui a été sauvé précédement par la glissiere
+		if (coeff) coeff /= 50;
+		else       coeff = 2;
+		var decalv=30, decalh=30;
+
+		var detecteCollisionTrou = function (pos0, pos1) {	// fonction volée à feldspath et Vapulabehemot, merci à eux (voir calc_inter dans Trajet_Gowap)
+			//var res = false
+			var x0 = pos0.x;
+			var y0 = pos0.x;
+			var tmax = Math.max(Math.abs(pos1.x - pos0.x), Math.abs(pos1.y - pos0.y));
+			var px = Math.sign(pos1.x - pos0.x);
+			var py = Math.sign(pos1.y - pos0.y);
+			var a = 0, b = 0, c = 0, delta = 0, t0 = 0, t1 = 0;
+			//window.console.log('verif collision gowap-trou [x0=' + x0 + ',y0=' + y0 + ', px=' + px + ', py=' + py + ', tmax=' + tmax + ']');
+
+			for(var k in position_trous_MZ) {
+				a = parseFloat(px*px+py*py);
+				b = parseFloat((x0-position_trous_MZ[k][0])*px+(y0-position_trous_MZ[k][1])*py);
+				c = parseFloat((x0-position_trous_MZ[k][0])*(x0-position_trous_MZ[k][0])+(y0-position_trous_MZ[k][1])*(y0-position_trous_MZ[k][1])-position_trous_MZ[k][2]);
+				delta = b*b-a*c
+				if(delta >= 0) {
+					t0 = Math.ceil(-b/a-Math.sqrt(delta)/a);
+					t1 = Math.floor(-b/a+Math.sqrt(delta)/a);
+					if(t0 <= tmax && t1 >= 0) {
+						// Roule' 10/10/2016 J'ai déplacé le flag res=true à l'intérieur de la boucle for ci-dessous car il y avait de fausses détections
+						//res = true;
+						//window.console.log('***** collision gowap-trou [x0=' + x0 + ',y0=' + y0 + ', px=' + px + ', py=' + py + ', tmax=' + tmax + ']');
+						for(var l=Math.max(0,t0); l<=Math.min(tmax,t1); l++) {
+							//window.console.log('***** collision gowap-trou en ' + (x0+l*px) + ', ' + (y0+l*py));
+							// Roule : pas utile pour nous
+							//res = true;
+							//chute.push([x0+l*px, y0+l*py]);
+							return true;
+						}
+					}
+				}
+			}
+			//return res;
+			return false;
+		};
+
+		var dessine_carte = function () {
+			dessin.width = 200*coeff+2*decalh;
+			dessin.height = 200*coeff+2*decalv;
+
+			//repere
+			ctx.beginPath();
+			ctx.moveTo(coord_x(0), coord_y(100));
+			ctx.lineTo(coord_x(0), coord_y(-100));
+			ctx.moveTo(coord_x(-100), coord_y(0));
+			ctx.lineTo(coord_x(100), coord_y(0));
+			ctx.stroke();
+			ctx.strokeRect(coord_x(-100), coord_y(100), coord_x(100) - coord_x(-100), coord_y(-100) - coord_y(100));
+
+			//trous
+			ctx.fillStyle = couleur_trou;
+			for(var i in position_trous_MZ) {
+				ctx.beginPath();
+				ctx.arc(coord_x(position_trous_MZ[i][0]), coord_y(position_trous_MZ[i][1]), coeff*position_trous_MZ[i][3], 0, Math.PI*2,  true);
+				ctx.fill();
+			}
+			// trajets
+			ctx.lineCap = "round";
+			ctx.lineJoin = "round";
+			for (var iSuivant in tabDepl) {
+				//window.console.log('carte_MZ, suivant n°' + iSuivant);
+				var tabDeplOneSuivant = tabDepl[iSuivant];
+				var x0 = coord_x(tabDeplOneSuivant[0].x);
+				var y0 = coord_y(tabDeplOneSuivant[0].y);
+				// La "cible" au départ
+				ctx.beginPath();
+				ctx.lineWidth = 1;
+				ctx.strokeStyle = couleur_cible;
+				ctx.fillStyle = couleur_cible;
+				ctx.arc(x0, y0, coeff * 4, 0, Math.PI*2,  true);
+				ctx.moveTo(x0 + coeff * 4, y0);
+				ctx.lineTo(x0 - coeff * 4, y0);
+				ctx.moveTo(x0, y0 + coeff * 4);
+				ctx.lineTo(x0, y0 - coeff * 4);
+				ctx.stroke();
+				// les segments
+				var nb_pts = tabDeplOneSuivant.length;
+				var pointPrecedent = tabDeplOneSuivant[0];
+				for (var i=1; i<nb_pts; i++) {
+					ctx.beginPath();
+					ctx.lineWidth = coeff;
+					ctx.moveTo(coord_x(pointPrecedent.x), coord_y(pointPrecedent.y));
+					ctx.strokeStyle = couleur_depl_normal;
+					var pointIntermediaire = pointIntermediaireMonstre2D(pointPrecedent, tabDeplOneSuivant[i]);
+					if (pointIntermediaire === undefined) {
+						if (detecteCollisionTrou(pointPrecedent, tabDeplOneSuivant[i]))
+							ctx.strokeStyle = couleur_depl_collision_trou;
+						else
+							ctx.strokeStyle = couleur_depl_normal;
+						ctx.lineTo(coord_x(tabDeplOneSuivant[i].x), coord_y(tabDeplOneSuivant[i].y));
+					} else {
+						if (detecteCollisionTrou(pointPrecedent, pointIntermediaire)
+								|| detecteCollisionTrou(pointIntermediaire, tabDeplOneSuivant[i]))
+							ctx.strokeStyle = couleur_depl_collision_trou;
+						else
+							ctx.strokeStyle = couleur_depl_normal;
+						ctx.lineTo(coord_x(pointIntermediaire.x), coord_y(pointIntermediaire.y));
+						ctx.lineTo(coord_x(tabDeplOneSuivant[i].x), coord_y(tabDeplOneSuivant[i].y));
+					}
+					pointPrecedent = tabDeplOneSuivant[i];
+					ctx.stroke();
+				}
+				// Les points à chaque étape
+				ctx.fillStyle = couleur_depl_normal;
+				for (var i=1; i<nb_pts; i++) {
+					ctx.beginPath();
+					var x = coord_x(tabDeplOneSuivant[i].x);
+					var y = coord_y(tabDeplOneSuivant[i].y);
+					ctx.arc(x, y, coeff, 0, Math.PI*2,  true);
+					ctx.fill();
+				}
+			}
+		}
+
+		this.setZoom = function (val) {
+			ctx.clearRect(0, 0, dessin.width, dessin.height);
+			coeff = val / 50;
+			dessine_carte();
+		}
+
+		// glissiere
+		var gliss = new glissiere_MZ(ref, "Zoom\u00A0:", this, true, 100, 50, 200);
+		var eGliss = gliss.getElt();
+		eGliss.style.position = 'absolute';
+		eGliss.style.top = (coeff * 2) + 'px';
+		eGliss.style.left = decalh + 'px';
+		dessin.style.position = 'relative';
+		div_carte.style.position = 'relative';
+		eGliss.style.zIndex = 9000;
+		div_carte.appendChild(eGliss);
+
+		// affichage au survol de la souris
+		var bulle = document.createElement('div');
+		bulle.style.visibility = 'hidden';
+		bulle.style.position = 'absolute';
+		bulle.style.zIndex = 3100;
+		bulle.style.width = '400px';
+		bulle.style.border = 'solid 1px #a1927f';
+		appendText(bulle, 'ici');
+		div_carte.appendChild(bulle);
+		var affichePosition = function(evt) {
+			if (evt.offsetX) {
+				var xsouris = evt.offsetX;
+				var ysouris = evt.offsetY;
+				var xpos = evt.clientX;
+				var ypos = evt.clientY + document.body.scrollTop;
+			}
+			else {
+				var xsouris = evt.layerX;
+				var ysouris = evt.layerY;
+				var xpos = evt.pageX;
+				var ypos = evt.pageY;
+			}
+			bulle.firstChild.nodeValue = xsouris;
+			bulle.style.top = ysouris + 'px';
+			bulle.style.left = xsouris + 'px';
+		}
+		addEvent(dessin, "mousemove", affichePosition, true);
+		addEvent(dessin, "mouseout", function() { bulle.style.visibility = 'hidden' }, true);
+		addEvent(dessin, "mouseover",  function() { bulle.style.visibility = 'visible' }, true);
+
+		// dessin initial
+		dessine_carte();
+
+		this.getElt = function() {return div_carte;};
+
+	} catch (e) {window.console.log(traceStack(e, 'carte_MZ'))}
 }
 
 
@@ -3741,6 +4006,33 @@ function do_equipgowap() {
 
 // x~x ordresgowap
 
+// version Roule' janvier 2017
+function setCarteUnGogoHTML5() {
+	// fabriquer la liste des positions successives
+	var listeDepl = [];	// ce sera un tableau d'objets
+	// position courrante
+	var eTitle = document.getElementById('titre2');
+	var tabPos = document.evaluate(".//table/descendant::table/tbody/tr/td/text()[contains(.,'X =')]", eTitle.parentNode, null, 9, null).singleNodeValue.nodeValue.match(/-?\d+/g);
+	listeDepl.push({x: parseInt(tabPos[0]), y: parseInt(tabPos[1]), n: parseInt(tabPos[2])});	// ParseInt obligatoire, javascript language de m*rd*
+	// déplacements
+	var lignes = eTitle.parentNode.getElementsByTagName('tr');
+	for(var i=0 ; i<lignes.length ; i++) {
+		if(lignes[i].className == 'mh_tdpage_fo') {
+			if (lignes[i].getElementsByTagName('td')[2] === undefined) return;	// cas des Golems
+			var point = lignes[i].getElementsByTagName('td')[2].firstChild.nodeValue.match(/X=(-?\d+) \| Y=(-?\d+) \| N=(-?\d+)/i);
+			if(point) listeDepl.push({x: parseInt(point[1]), y: parseInt(point[2]), n: parseInt(point[3])});
+		}
+	}
+	// générer la carte
+	var carte = new carte_MZ('cartegogo', [listeDepl]);	// Le 2e arg est un tableau de tableaux d'objets
+	// positionner la carte
+	var footer = document.getElementById('footer1');
+	var eCarte = carte.getElt();
+	eCarte.style.textAlign = 'left';
+	if (footer) footer.parentNode.insertBefore(eCarte, footer);
+	else document.body.appendChild(eCarte);
+}
+
 /* v0.2 by Dab - 2013-08-31
  * - correction acquisition posale initiale
  * - correction tare et affichage mire
@@ -3800,11 +4092,43 @@ function testeGlissiere() {
 	} catch (e) {window.console.log(traceStack(e, 'testeGlissiere'))};
 }
 
-function do_ordresgowap() {
-	setCarteGogo();		// Via script des trouillots
-	if (isDEV) {
-		testeGlissiere();
+function MZ_testsUnitairesCalculIntermediaire(x0, y0, x1, y1) {
+	if (x0 !== undefined) {
+		// ouais, récursif une fois
+		var PtInterm = pointIntermediaireMonstre2D({x:x0, y:y0}, {x:x1, y:y1});
+		if (PtInterm === undefined) 
+			window.console.log('pt interm(' + x0 + ',' + y0 + ')(' + x1 + ',' + y1 + ') => rien');
+		else
+			window.console.log('pt interm(' + x0 + ',' + y0 + ')(' + x1 + ',' + y1 + ') => (' + PtInterm.x + ',' + PtInterm.y +')');
+		return;
 	}
+	// MZ_testsUnitairesCalculIntermediaire(10, 10, 100, 100);
+	// MZ_testsUnitairesCalculIntermediaire(10, 100, 100, 10);
+	// MZ_testsUnitairesCalculIntermediaire(100, 10, 10, 100);
+	// MZ_testsUnitairesCalculIntermediaire(100, 100, 10, 10);
+	MZ_testsUnitairesCalculIntermediaire(10, 10, 100, 80);
+	MZ_testsUnitairesCalculIntermediaire(10, 10, 80, 100);
+	MZ_testsUnitairesCalculIntermediaire(10, 100, 80, 10);
+	MZ_testsUnitairesCalculIntermediaire(10, 80, 100, 10);
+	MZ_testsUnitairesCalculIntermediaire(100, 10, 10, 80);
+	MZ_testsUnitairesCalculIntermediaire(80, 10, 10, 100);
+	MZ_testsUnitairesCalculIntermediaire(100, 80, 10, 10);
+	MZ_testsUnitairesCalculIntermediaire(80, 100, 10, 10);
+	MZ_testsUnitairesCalculIntermediaire(-80, 100, -10, 10);
+	MZ_testsUnitairesCalculIntermediaire(80, -100, 10, -10);
+	MZ_testsUnitairesCalculIntermediaire(-80, -100, -10, -10);
+	MZ_testsUnitairesCalculIntermediaire(35, -87, 45, -87);
+	MZ_testsUnitairesCalculIntermediaire(45, -87, 45, -77);
+}
+
+function do_ordresgowap() {
+	if (isDEV) {
+		setCarteUnGogoHTML5();		// Version Roule janvier 2017
+		//testeGlissiere();
+		//MZ_testsUnitairesCalculIntermediaire();
+	} else {
+	}
+	setCarteGogo();		// Via script des trouillots
 }
 
 /*********************************************************************************
@@ -11559,10 +11883,5 @@ try {
 		do_profil2();
 	}
 } catch(e) {
-	if (e.stack) {
-		var sStack = e.stack;
-		// enlever les infos confidentielles
-		sStack =  sStack.replace(/file\:\/\/.*gm_scripts/ig, '...');
-	} else {var sStack = '';}
 	window.console.log(traceStack(e, 'catch général page ' + window.location.pathname));
 }
