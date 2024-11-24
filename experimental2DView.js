fd911b52845b49d5d9c9347cf84d01e3239ba122
Frédéric Donckels <frederic.donckels+troogle@gmail.com>
2024-11-24 10:59:33 +0100

    indexage de la carte

--- experimental2DView.js
+++ experimental2DView.js
@@ -1,7 +1,6 @@
-
 class Grid {
 
-    constructor(x,y,n, horizontalRange, verticalRange) {
+    constructor(x, y, n, horizontalRange, verticalRange) {
         this.centerX = x;
         this.centerY = y;
         this.centerN = n;
@@ -22,10 +21,8 @@ class Grid {
     getCell(x, y) {
         let i = this.xToIndex(x);
         let j = this.yToIndex(y);
-        if ( i > 1 + 2 * this.horizontalRange || i < 0
-            || j > 1 + 2 * this.horizontalRange || j < 0
-        ) {
-            console.log(`Out of range: ${x} ${y}  => ${i} ${j}`);
+        if (i > 1 + 2 * this.horizontalRange || i < 0
+            || j > 1 + 2 * this.horizontalRange || j < 0) {
             // outside of the view range
             return null;
         }
@@ -36,16 +33,72 @@ class Grid {
         }
         let cell = column[j];
         if (null == cell) {
-            //cell = new Cell();
-            cell = [];
+            cell = new Cell(x,y);
             column[j] = cell;
         }
-        console.log(cell);
         return cell;
     }
+
+    indexMap(monsters, trolls, treasures, places, mushrooms, graves) {
+        this.indexCategory(monsters, (cell, o) => cell.addMonster(o));
+        this.indexCategory(trolls, (cell, o) => cell.addTroll(o));
+        this.indexCategory(treasures, (cell, o) => cell.addTreasure(o));
+        this.indexCategory(places, (cell, o) => cell.addPlace(o));
+        this.indexCategory(mushrooms, (cell, o) => cell.addMushroom(o));
+        this.indexCategory(graves, (cell, o) => cell.addGrave(o));
+    }
+
+    indexCategory(gridElements, addFunction) {
+        if (null == gridElements) {
+            return;
+        }
+        for (const element of gridElements) {
+            let o = new CellObject(element);
+            let cell = this.getCell(o.x, o.y)
+            if (null != cell) {
+                addFunction(cell, o);
+            }
+        }
+    }
+
 }
 
 class Cell {
+
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    addMonster(monster) {
+        this.monsters = null == this.monsters ? [] : this.monsters;
+        this.monsters.push(monster);
+    }
+
+    addTroll(troll) {
+        this.trolls = null == this.trolls ? [] : this.trolls;
+        this.trolls.push(troll);
+    }
+
+    addMushroom(mushroom) {
+        this.mushrooms = null == this.mushrooms ? [] : this.mushrooms;
+        this.mushrooms.push(mushroom);
+    }
+
+    addGrave(grave) {
+        this.graves = null == this.graves ? [] : this.graves;
+        this.graves.push(grave);
+    }
+
+    addPlace(place) {
+        this.places = null == this.places ? [] : this.places;
+        this.places.push(place);
+    }
+
+    addTreasure(treasure) {
+        this.treasures = null == this.treasures ? [] : this.treasures;
+        this.treasures.push(treasure);
+    }
 
 }
 
@@ -54,30 +107,42 @@ class CellObject {
     constructor(mhValue) {
         let val = mhValue.value;
         this.type = val.type;
-        this.family = "todo";
-        this.name = val.nom.options.sortValue;
         this.id = val.id;
         this.x = val.x;
         this.y = val.y;
         this.n = val.n;
-        this.groupName = "todo";
         this.distance = val.dist; // todo
         this.distanceCost = 0; // todo
-    }
-}
+
+        switch (this.type) {
+            case "monstres" :
+                this.groupName = "todo";
+                this.family = "todo";
+                this.name = val.nom.options.sortValue;
+                break;
+            case "champignons":
+                this.name = val.nom;
+                break;
+            case "cenotaphes" :
+                // TODO
+                break;
+            case "lieux" :
+                this.name = val.nom.options.sortValue;
+                break;
+            case "tresors" :
+                this.name = val.nom.options.sortValue;
+                break;
+            case "trolls" :
+                this.name = val.nom.options.sortValue;
+                this.race = val.race;
+                this.level = val.niv;
+                break;
 
 
-function indexMap(grid, monstres) {
-    for (const mob of monstres) {
-        let monstre = new CellObject(mob);
-        let cell = grid.getCell(monstre.x, monstre.y)
-        if (null == cell) {
-        } else {
-            cell.push(monstre);
         }
     }
 }
 
 
-g = new Grid(86,78,-30,14,7);
-indexMap(g, json_monstres);
+g = new Grid(86, 78, -30, 14, 7);
+g.indexMap(json_monstres, json_trolls, json_tresors, json_lieux, json_champignons, json_cenotaphes);
