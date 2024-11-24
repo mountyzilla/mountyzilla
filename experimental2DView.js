9d79b3a84e210e3418fe7102f6345323071a3442
Frédéric Donckels <frederic.donckels+troogle@gmail.com>
2024-11-24 14:09:22 +0100

    début de vue 2D

--- experimental2DView.js
+++ experimental2DView.js
@@ -6,8 +6,35 @@ class Grid {
         this.centerN = n;
         this.horizontalRange = horizontalRange;
         this.verticalRange = verticalRange;
+        this.gridSize = 1 + 2 * this.horizontalRange;
 
-        this.cells = new Array(1 + 2 * horizontalRange);
+        this.cells = new Array(this.gridSize);
+    }
+
+    convertToHtml() {
+        let html = `<div style="display: grid; column-gap: 2px; row-gap: 2px;"> `;
+        for (let i = 0; i < this.gridSize; i++) {
+            let column = this.cells[i];
+            for (let j = 0; j < this.gridSize; j++) {
+                if (null == column) {
+                    html += this.emptyCell(i, j);
+                    continue;
+                }
+                let cell = column[j];
+                if (null == cell) {
+                    html += this.emptyCell(i, j);
+                    continue;
+                }
+                html += cell.convertToHtml(i, j, this.centerX, this.centerY);
+            }
+        }
+        html += `</div>`;
+        return html;
+    }
+
+    emptyCell(i, j) {
+        const center = 1 + this.horizontalRange;
+        return `<div style="grid-row-start: ${i + 1}; grid-column-start: ${j + 1}" class="${cellStyle(center, center, i+1, j+1)}">&nbsp;</div>`;
     }
 
     xToIndex(x) {
@@ -21,19 +48,19 @@ class Grid {
     getCell(x, y) {
         let i = this.xToIndex(x);
         let j = this.yToIndex(y);
-        if (i > 1 + 2 * this.horizontalRange || i < 0
-            || j > 1 + 2 * this.horizontalRange || j < 0) {
+        if (i > this.gridSize || i < 0
+            || j > this.gridSize || j < 0) {
             // outside of the view range
             return null;
         }
         let column = this.cells[i];
         if (null == column) {
-            column = new Array(1 + 2 * this.horizontalRange);
+            column = new Array(this.gridSize);
             this.cells[i] = column;
         }
         let cell = column[j];
         if (null == cell) {
-            cell = new Cell(x,y);
+            cell = new Cell(x, y);
             column[j] = cell;
         }
         return cell;
@@ -63,41 +90,71 @@ class Grid {
 
 }
 
+function cellStyle(centerX, centerY, x, y) {
+    const distX = Math.abs(centerX - x);
+    const distY = Math.abs(centerY - y);
+    const dist = Math.max(distX, distY);
+    console.log(`${centerX}_${centerX}_${x}_${y}_${distX}_${distY}`);
+    return 0 === (dist % 2) ? `mh_view_odd` : `mh_view_even`;
+}
+
 class Cell {
 
-    constructor(x,y) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
+    }
+
+    convertToHtml(i, j, centerX, centerY) {
+        let html = `<div style="grid-row-start: ${i + 1}; grid-column-start: ${j + 1}" class="${cellStyle(centerX, centerY, this.x, this.y)}">`;
+        if (null != this.monsters) {
+            for (const monster of this.monsters) {
+                html += `${monster.name}<br/>`;
+            }
+        }
+        if (null != this.trolls) {
+            for (const troll of this.trolls) {
+                html += `${troll.name}<br/>`;
+            }
+        }
+        html += "</div>";
+        return html;
     }
 
     addMonster(monster) {
         this.monsters = null == this.monsters ? [] : this.monsters;
         this.monsters.push(monster);
+        this.monsters.sort((a, b) => a.n - b.n);
     }
 
     addTroll(troll) {
         this.trolls = null == this.trolls ? [] : this.trolls;
         this.trolls.push(troll);
+        this.trolls.sort((a, b) => a.n - b.n);
     }
 
     addMushroom(mushroom) {
         this.mushrooms = null == this.mushrooms ? [] : this.mushrooms;
         this.mushrooms.push(mushroom);
+        this.mushrooms.sort((a, b) => a.n - b.n);
     }
 
     addGrave(grave) {
         this.graves = null == this.graves ? [] : this.graves;
         this.graves.push(grave);
+        this.graves.sort((a, b) => a.n - b.n);
     }
 
     addPlace(place) {
         this.places = null == this.places ? [] : this.places;
         this.places.push(place);
+        this.places.sort((a, b) => a.n - b.n);
     }
 
     addTreasure(treasure) {
         this.treasures = null == this.treasures ? [] : this.treasures;
         this.treasures.push(treasure);
+        this.treasures.sort((a, b) => a.n - b.n);
     }
 
 }
@@ -111,14 +168,13 @@ class CellObject {
         this.x = val.x;
         this.y = val.y;
         this.n = val.n;
-        this.distance = val.dist; // todo
-        this.distanceCost = 0; // todo
+        this.distance = val.dist;
 
         switch (this.type) {
             case "monstres" :
-                this.groupName = "todo";
-                this.family = "todo";
                 this.name = val.nom.options.sortValue;
+                this.groupName = this.toGroupName(this.name);
+                this.family = "todo";
                 break;
             case "champignons":
                 this.name = val.nom;
@@ -141,8 +197,27 @@ class CellObject {
 
         }
     }
+
+    toGroupName(name) {
+        let i = name.indexOf("[");
+        if (i < 0) {
+            return name;
+        }
+        let j = i - 1;
+        while (j > 0 && ' ' === name[j]) j--;
+        return name.substring(0, j + 1);
+    }
 }
 
 
+const style = document.createElement('style');
+style.appendChild(document.createTextNode(`
+.mh_view_odd { background-color: teal; }
+.mh_view_even { background-color: lightgreen; }
+`));
+document.head.appendChild(style);
+
 g = new Grid(86, 78, -30, 14, 7);
 g.indexMap(json_monstres, json_trolls, json_tresors, json_lieux, json_champignons, json_cenotaphes);
+html = g.convertToHtml();
+$('#infoTab').after(html);
