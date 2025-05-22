984480f6340a10f9af80feecaf50fb331a78e60c
alainarachnid <alainarachnid@brion.fr>
2025-05-22 18:54:09 +0200

    Fix plantage quand il n'y a pas de conf centralisée

--- Tout_MZ.user.js
+++ Tout_MZ.user.js
@@ -10,7 +10,7 @@
 // @exclude     *mh2.mh.raistlin.fr*
 // @exclude     *mhp.mh.raistlin.fr*
 // @exclude     *mzdev.mh.raistlin.fr*
-// @version     1.6.61
+// @version     1.6.62
 // @grant GM_getValue
 // @grant GM_deleteValue
 // @grant GM_setValue
@@ -36,7 +36,7 @@
 *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *
 *******************************************************************************/
 
-var MZ_latest = '1.6.61';
+var MZ_latest = '1.6.62';
 var MZ_changeLog = [
 	"V1.6.x \t\t 23/12/2024",
 	"	- Adapations nouvelle vue",
@@ -14646,6 +14646,7 @@ class MZ_cLigneMonstre extends MZ_cLigneVue {
 		document.getElementById('MZ_nivMinMonstres').value = '';
 		document.getElementById('MZ_nivMaxMonstres').value = '';
 		document.getElementById('MZ_FamilleMonstres').value = '';
+		document.getElementById('MZ_cacheMonstre').value = '';
 		for (let nomfiltre in MZ_cLigneMonstre.nomsFiltres)
 			document.getElementById('MZ_chkMonstre' + nomfiltre).checked = false;
 	}
@@ -14759,7 +14760,7 @@ class MZ_cLigneMonstre extends MZ_cLigneVue {
 		let tReq = [];
 		let nbReq = 0;
 		let nbMax = 500;
-		if (!bFull && MH_mountyzilla_json.general) {
+		if (!bFull && MH_mountyzilla_json && MH_mountyzilla_json.general) {
 			nbMax = MH_mountyzilla_json.general.maxRecupCdM;
 			if (nbMax == undefined) nbMax = 500;
 		}
