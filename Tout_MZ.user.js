86c33a08a08830d0c0df8bc54a7ef78ba3bfee80
Dorian <dorloup@gmail.com>
2025-06-17 21:40:49 +0200

    mobile: meilleure lisibilité équipement + reset vue incorrect

--- Tout_MZ.user.js
+++ Tout_MZ.user.js
@@ -10,7 +10,7 @@
 // @exclude     *mh2.mh.raistlin.fr*
 // @exclude     *mhp.mh.raistlin.fr*
 // @exclude     *mzdev.mh.raistlin.fr*
-// @version     1.6.73
+// @version     1.6.74
 // @grant GM_getValue
 // @grant GM_deleteValue
 // @grant GM_setValue
@@ -36,7 +36,7 @@
 *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *
 *******************************************************************************/
 
-var MZ_latest = '1.6.73';
+var MZ_latest = '1.6.74';
 var MZ_changeLog = [
 	"V1.6.x \t\t 23/12/2024",
 	"	- Adapations nouvelle vue",
@@ -9628,9 +9628,23 @@ function traiteMinerai() {
 	td.colSpan = 7;
 }
 
+function visuMobileTableaux() {
+	if (isDesktopView()) return;
+
+	let numTroll = MY_getValue('NUM_TROLL');
+	let tables = ['Parchemin', 'Potion', 'Spécial'];
+	for (let section of tables) {
+		$(`#part2toggle_${numTroll}_${section}`).find('th:last-child, td:last-child').remove()
+		if (section == 'Spécial') continue;  // détails peu intéressant ici
+		let thDetail = document.querySelector(`#part2toggle_${numTroll}_${section}>table>thead>tr>th:nth-child(4)`);
+		thDetail.style = 'min-width: 185px;';
+	}
+}
+
 function do_equip() {
 	start_script(undefined, 'do_equip_log');
 
+	visuMobileTableaux();
 	traiteChampis();
 	traiteCompos();
 	traiteMinerai();
@@ -11308,9 +11322,9 @@ class MZ_cVueExterne {
 			let vueext = MY_getValue('VUEEXT');
 			// vue par défaut (celle que Roule préfère ;)
 			if (!vueext) vueext = "Bricol' Vue";
-			// protection contre mauvaise valeur (ne devrait pas arriver)
 			if (!MZ_cVueExterne.vue2Ddata[vueext]) {
 				logMZ(`vue externe non valide: ${vueext}`);
+				MY_setValue('VUEEXT', 'Bricol\' Vue');  // reset à une vue valide
 				return;
 			}
 
