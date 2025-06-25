195f13d03b9861c65b66ed7295feef7b692d76f6
Dorian <dorloup@gmail.com>
2025-06-25 14:02:22 +0200

    sciz: surcharge events in news

--- Tout_MZ.user.js
+++ Tout_MZ.user.js
@@ -10,7 +10,7 @@
 // @exclude     *mh2.mh.raistlin.fr*
 // @exclude     *mhp.mh.raistlin.fr*
 // @exclude     *mzdev.mh.raistlin.fr*
-// @version     1.6.75
+// @version     1.6.76
 // @grant GM_getValue
 // @grant GM_deleteValue
 // @grant GM_setValue
@@ -36,7 +36,7 @@
 *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *
 *******************************************************************************/
 
-var MZ_latest = '1.6.75';
+var MZ_latest = '1.6.76';
 var MZ_changeLog = [
 	"V1.6.x \t\t 23/12/2024",
 	"	- Adapations nouvelle vue",
@@ -16756,6 +16756,7 @@ try {
 	} else if (isPage("MH_Play/Play_vue")) {
 		do_vue();
 	} else if (isPage("MH_Play/Play_news")) {
+		MZ_cSCIZ.init()._overwriteEvents()
 		do_news();
 	} else if (isPage("MH_Play/Play_evenement")) {
 		MZ_cSCIZ.init()._overwriteEvents()
