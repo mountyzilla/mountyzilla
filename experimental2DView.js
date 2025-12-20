4aa36b09e65aee71d89eca72b1b4b682a2647bbe
Frédéric Donckels <frederic.donckels@gmail.com>
2025-12-20 15:54:58 +0100

    Version++

--- experimental2DView.js
+++ experimental2DView.js
@@ -6,7 +6,7 @@
 // @exclude *mh2.mh.raistlin.fr*
 // @exclude *mzdev.mh.raistlin.fr*
 // @name Vue2D
-// @version 0.5.0
+// @version 0.6.0
 // @namespace https://greasyfork.org/en/users/1536460
 // @downloadURL https://update.greasyfork.org/scripts/555450/Vue2D.user.js
 // @updateURL https://update.greasyfork.org/scripts/555450/Vue2D.user.js
