6b44028247742fb1ea1cd9fa162ccb9006bac006
alainarachnid <alainarachnid@brion.fr>
2026-01-10 21:20:43 +0100

    Fix pb encodage qui n'apparait qu'avec l'injecteur

--- Tout_MZ.user.js
+++ Tout_MZ.user.js
@@ -10,7 +10,7 @@
 // @exclude     *mh2.mh.raistlin.fr*
 // @exclude     *mhp.mh.raistlin.fr*
 // @exclude     *mzdev.mh.raistlin.fr*
-// @version     1.7.15
+// @version     1.7.16
 // @grant GM_getValue
 // @grant GM_deleteValue
 // @grant GM_setValue
@@ -36,7 +36,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *
  *******************************************************************************/
 
-var MZ_latest = '1.7.15';
+var MZ_latest = '1.7.16';
 var MZ_changeLog = [
     "V1.7.14 \t\t 30/11/2025",
     "	- Dans l'équipement, affichage des infos des cartes du trésor (script du Capitan)",
@@ -15439,7 +15439,7 @@ function MZ_texteAideSortileges(sort) {
                 Balayage: "Balayage",
                 Charge: "Charger",
                 CdB: "CdB1",
-                Frénésie: "Frenesie",
+                'Frénésie': "Frenesie",
                 RB: "RB1",
                 GdS: "GdS",
                 Siphon: "Siphon"
@@ -15512,7 +15512,7 @@ function MZ_texteAideSortileges(sort) {
                 Charge: "Charger",
                 CA: "CA",
                 CdB: "CdB1",
-                Frénésie: "Frenesie",
+                'Frénésie': "Frenesie",
                 RB: "RB1",
                 Rafale: "Rafale",
                 Vampi: "Vampi"
