092b168fdbc3413eebe5353c70871a748b081221
alainarachnid <alainarachnid@brion.fr>
2026-06-21 18:20:53 +0200

    permettre à la box des infos SCIZ de déborder en dessous

--- Tout_MZ.user.js
+++ Tout_MZ.user.js
@@ -10,7 +10,7 @@
 // @exclude     *mh2.mh.raistlin.fr*
 // @exclude     *mhp.mh.raistlin.fr*
 // @exclude     *mzdev.mh.raistlin.fr*
-// @version     1.7.37
+// @version     1.7.38
 // @grant GM_getValue
 // @grant GM_deleteValue
 // @grant GM_setValue
@@ -36,7 +36,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *
  *******************************************************************************/
 
-var MZ_latest = '1.7.37';
+var MZ_latest = '1.7.38';
 var MZ_changeLog = [
     "V1.7.17 \t\t 19/02/2026",
     "	- Pour nos amis K : AM : PV pour jouer tout de suite",
@@ -6876,6 +6876,10 @@ class MZ_cSCIZ {
         MZ_cSCIZ.monsters = [];  // reset view
         let mobs = [];
         let iMonster = 0;
+        try {   // permettre à la box des infos SCIZ de déborder en dessous
+            MZ_cVueJSON.oMonstres.eltTable.parentNode.style.overflow = 'visible';
+            MZ_cVueJSON.oMonstres.eltTable.parentNode.parentNode.style.overflow = 'visible';
+        } catch (e) {}
         for (let oLigne of MZ_cVueJSON.oMonstres.objets) {
             let mob = oLigne.nom.match(/(?:une*\s*)*([^<>]+?)\s*\[\s*([^\]]+)/);
             if (!mob) {
